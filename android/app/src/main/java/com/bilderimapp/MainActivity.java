59eb1a7db1a4b77a99ddb444e907a26af27a20af
Emre Güler <emre_guler01@hotmail.com>
2020-09-24 21:34:49 +0300

    rn-splash-screen

--- android/app/src/main/java/com/bilderimapp/MainActivity.java
+++ android/app/src/main/java/com/bilderimapp/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends ReactActivity {
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     SplashScreen.show(this);
+    super.onCreate(savedInstanceState);
   }
   @Override
   protected String getMainComponentName() {
